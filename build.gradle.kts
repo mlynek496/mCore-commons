bd8b7bb339c5559d64670dee51edddfe9f3c05e2
mlynek496 <mlynusmlynus@gmail.com>
2026-08-28 19:03:12 +0200

    new version v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,11 @@ repositories {
     maven("https://repo.panda-lang.org/releases")
     maven("https://repo.okaeri.cloud/releases")
     maven("https://storehouse.okaeri.eu/repository/maven-public/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     implementation("net.kyori:adventure-api:5.2.0")
     compileOnly("org.projectlombok:lombok:1.18.32")
     annotationProcessor("org.projectlombok:lombok:1.18.32")
@@ -25,7 +26,6 @@ dependencies {
     compileOnly("dev.triumphteam:triumph-gui:3.1.13")
     compileOnly("eu.okaeri:okaeri-configs-yaml-bukkit:5.0.13")
     compileOnly("eu.okaeri:okaeri-configs-serdes-bukkit:5.0.13")
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.13")
     compileOnly("com.mojang:authlib:1.5.25")
     compileOnly("me.clip:placeholderapi:2.12.2")
