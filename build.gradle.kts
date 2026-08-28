656bca9f84b50b9d6f47ddc72046e1c03bed961c
mlynek496 <mlynusmlynus@gmail.com>
2026-08-28 02:13:05 +0200

    new version v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,26 +8,24 @@ version = "1.0.5"
 
 repositories {
     mavenCentral()
-    maven("https://jitpack.io")
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://maven.enginehub.org/repo/")
     maven("https://repo.helpch.at/releases")
     maven("https://repo.panda-lang.org/releases")
     maven("https://repo.okaeri.cloud/releases")
     maven("https://storehouse.okaeri.eu/repository/maven-public/")
-    maven("https://projectlombok.org/edge-releases")
-    maven("https://repo.eternalcode.pl/releases")
-    maven("https://repo.triumphteam.dev/snapshots/")
 }
 
 dependencies {
-    api("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.13")
     compileOnly("com.mojang:authlib:1.5.25")
     compileOnly("me.clip:placeholderapi:2.12.2")
-    compileOnly("org.projectlombok:lombok:1.18.32")
-    annotationProcessor("org.projectlombok:lombok:1.18.32")
+    implementation("net.kyori:adventure-api:5.2.0")
     implementation("dev.rollczi:litecommands-bukkit:3.10.9")
+    implementation("dev.rollczi:litecommands-velocity:3.10.9")
     implementation("dev.morphia.morphia:morphia-core:2.5.3")
     implementation("dev.triumphteam:triumph-gui:3.1.13")
     implementation("eu.okaeri:okaeri-configs-yaml-bukkit:5.0.13")
