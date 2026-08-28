fb6cb7ee0bfd68b1ced76d9aee122688bbbda979
mlynek496 <mlynusmlynus@gmail.com>
2026-08-28 16:51:40 +0200

    new version v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,27 +10,25 @@ repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://maven.enginehub.org/repo/")
-    maven("https://repo.helpch.at/releases")
     maven("https://repo.panda-lang.org/releases")
     maven("https://repo.okaeri.cloud/releases")
     maven("https://storehouse.okaeri.eu/repository/maven-public/")
 }
 
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    implementation("net.kyori:adventure-api:5.2.0")
     compileOnly("org.projectlombok:lombok:1.18.32")
     annotationProcessor("org.projectlombok:lombok:1.18.32")
-    api("net.kyori:adventure-api:4.17.0")
-    api("net.kyori:adventure-text-serializer-legacy:4.17.0")
-    api("dev.rollczi:litecommands-bukkit:3.10.9")
-    api("dev.rollczi:litecommands-velocity:3.10.9")
-    api("dev.morphia.morphia:morphia-core:2.5.3")
-    api("dev.triumphteam:triumph-gui:3.1.13")
-    api("eu.okaeri:okaeri-configs-yaml-bukkit:5.0.13")
-    api("eu.okaeri:okaeri-configs-serdes-bukkit:5.0.13")
-    compileOnlyApi("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
-    compileOnlyApi("com.sk89q.worldguard:worldguard-bukkit:7.0.13")
-    compileOnlyApi("com.mojang:authlib:1.5.25")
-    compileOnlyApi("me.clip:placeholderapi:2.12.2")
+    compileOnly("dev.rollczi:litecommands-bukkit:3.10.9")
+    compileOnly("dev.morphia.morphia:morphia-core:2.5.3")
+    compileOnly("dev.triumphteam:triumph-gui:3.1.13")
+    compileOnly("eu.okaeri:okaeri-configs-yaml-bukkit:5.0.13")
+    compileOnly("eu.okaeri:okaeri-configs-serdes-bukkit:5.0.13")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.13")
+    compileOnly("com.mojang:authlib:1.5.25")
+    compileOnly("me.clip:placeholderapi:2.12.2")
 }
 
 java {
