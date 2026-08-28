501da1f5bc93efd06ca777c2549a68c89f0cdc8b
mlynek496 <mlynusmlynus@gmail.com>
2026-08-28 16:46:57 +0200

    new version v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,16 +21,16 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.32")
     api("net.kyori:adventure-api:4.17.0")
     api("net.kyori:adventure-text-serializer-legacy:4.17.0")
+    api("dev.rollczi:litecommands-bukkit:3.10.9")
+    api("dev.rollczi:litecommands-velocity:3.10.9")
+    api("dev.morphia.morphia:morphia-core:2.5.3")
+    api("dev.triumphteam:triumph-gui:3.1.13")
+    api("eu.okaeri:okaeri-configs-yaml-bukkit:5.0.13")
+    api("eu.okaeri:okaeri-configs-serdes-bukkit:5.0.13")
     compileOnlyApi("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnlyApi("com.sk89q.worldguard:worldguard-bukkit:7.0.13")
     compileOnlyApi("com.mojang:authlib:1.5.25")
     compileOnlyApi("me.clip:placeholderapi:2.12.2")
-    implementation("dev.rollczi:litecommands-bukkit:3.10.9")
-    implementation("dev.rollczi:litecommands-velocity:3.10.9")
-    implementation("dev.morphia.morphia:morphia-core:2.5.3")
-    implementation("dev.triumphteam:triumph-gui:3.1.13")
-    implementation("eu.okaeri:okaeri-configs-yaml-bukkit:5.0.13")
-    implementation("eu.okaeri:okaeri-configs-serdes-bukkit:5.0.13")
 }
 
 java {
